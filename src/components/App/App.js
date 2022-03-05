c062ee916e8204e6c6a7d51119bf7e607cea1406
cesmc <xccuevas@gmail.com>
2022-03-04 18:31:52 -0600

    feat(app): create app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,8 +4,22 @@ function App(props) {
   const { title } = props;
 
   return (
-    <div>
-      <p>{title}</p>
+    <div className='app-container'>
+      <h1 className='app-title'>{title}</h1>
+      <form className='form-container'>
+        <div className='form-token'>
+          <p className='form-title'>🔓 Token</p>
+          <input placeholder='You need a Banxio token.' />
+          <a href='https://www.banxico.org.mx/SieAPIRest/service/v1/token'>You can find it on.</a>
+        </div>
+        <div className='form-series'>
+          <p className='form-title'>🔑 Series</p>
+          <div className='form-series--input-button'>
+            <input className='form-series--input' placeholder='Example: SF61745,SP68257' />
+            <button className='form-button'>Fetch</button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
